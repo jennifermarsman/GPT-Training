67cc1e1ea7a627ad66f6943fab7053dc76ccbeb2
Jennifer Marsman <jennmar@microsoft.com>
2023-09-06 22:45:42 -0400

    Changed manual text to frying best practices with reference and prompt formatting fixes

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ from langchain.prompts import (
 
 # Constants for calling the Azure OpenAI service
 openai_api_type = "azure"
-gpt4_endpoint = "https://TODO.openai.azure.com/"            # Your endpoint will look something like  this: https://YOUR_AOAI_RESOURCE_NAME.openai.azure.com/
+gpt4_endpoint = "https://TODO.openai.azure.com/"            # Your endpoint will look something like this: https://YOUR_AOAI_RESOURCE_NAME.openai.azure.com/
 gpt4_api_key = "TODO"                                       # Your key will look something like this: 00000000000000000000000000000000
 gpt4_deployment_name="gpt-4"
 
@@ -28,8 +28,8 @@ def generate_quiz(rag_from_manual):
     system_message_prompt = SystemMessagePromptTemplate.from_template(system_template)
 
     user_prompt=PromptTemplate(
-        template="{rag_from_manual} \n" +
-                "## Question\n The above manual is for the ice cream machine at the restaurant. Write a quiz question for a new employee to test if they understand the above content. \n" +
+        template="## Manual \n {rag_from_manual} \n" +
+                "## Question \n The above manual is for equipment used at the restaurant. Write a quiz question for a new employee to test if they understand the above content. The multiple choice question should have at least one tricky answer.\n" +
                 "## Quiz Question \n",
         input_variables=["rag_from_manual"],
     )
@@ -52,10 +52,10 @@ def evaluate_quiz(excerpt, quiz, user_answer):
     system_message_prompt = SystemMessagePromptTemplate.from_template(system_template)
 
     user_prompt=PromptTemplate(
-        template="## Manual {excerpt} \n" +
-                "## Quiz Question {quiz} \n" +
-                "## User Answer {user_answer} \n" +
-                "## Question\n Given the above chat history, is the employee's answer corrrect?  Write a short assessment of how they did. \n" +
+        template="## Manual \n {excerpt} \n" +
+                "## Quiz Question \n {quiz} \n" +
+                "## User Answer \n {user_answer} \n" +
+                "## Question \n Given the above chat history, is the employee's answer correct?  Write a short assessment of how they did. \n" +
                 "## Assessment \n",
         input_variables=["excerpt", "quiz", "user_answer"],
     )
@@ -75,7 +75,9 @@ def evaluate_quiz(excerpt, quiz, user_answer):
 
 
 with gr.Blocks(css=".gradio-label {color: red}") as demo:
-    manual_text = "## Manual Winter Storage\n If the place of business is to be closed during the winter months, it is important to protect the freezer by following certain precautions, particularly if the building is subject to freezing conditions.  Important! On water-cooled freezers, disconnect the water supply. Relieve pressure on the spring in the water valve. Use air pressure on the outlet side to blow out any water remaining in the condenser. This is extremely important. Failure to follow this procedure may cause severe and costly damage to the refrigeration system. Your local Taylor distributor can perform this service for you.\n • Disconnect the freezer from the main power source to prevent possible electrical damage.\n • Wrap detachable parts of the freezer, such as the beater assembly and freezer door, and place them in a protected dry place. Rubber trim parts and gaskets can be protected by wrapping them with moisture-proof paper.\n • All parts should be thoroughly cleaned of dried mix or lubrication accumulations, which attract mice and other vermin."
+    # Manual for a deep fryer: https://static-pt.com/modelManual/SOU-14-18_spm.pdf?v=1532002599167  (page 13)
+    manual_text = "SOME DO’S AND DON’TS OF GOOD FRYING PRACTICES\n\nFRYING DO’S\n1. Make sure kettles are clean. \n2. Make sure thermostats are registering and functioning properly. \n3. Fill kettles only to proper frying level, which is 2% above the tubes when the frying fat is at frying temperature. A fry level line is stamped on the tank. \n4. Maintain proper level of frying fat in the kettle by occasional additions of fresh fat as the kettle fries down. \n5. Keep heating tubes covered at all times when heat is on. \n6. Fry at temperatures in the range of 325°F to 375°F. \n7. Turn heat in the kettles to 200”F, or preferable off between frying periods, or during any periods of time when this is practical. \n8. Fry foods in amounts only up to a full kettle load; a full kettle loading being the point where the temperature recovers to the dial setting and the thermostat turns off the burner before the food is completely fried or done. \n9. Remove food baskets from kettle as soon as food is done, allowing food to drain over kettle a minimum of 30 seconds. \n10. Keep fat as clean as possible at all times, removing immediately any floating burned particles. \n11. Make sure baskets are sound and don’t leak food into kettle. \n12. Drain kettle, filter fat and remove all residue from cold zone at least once daily. Boil out kettle and baskets with detergent at least once a week, scraping off any foreign materials not yielding to the treatment. Rinse kettle several times by filling with fresh water and bring to boil. Follow procedure under “MATNTENANCE” section. \n13. Keep kettles covered when not in use. \n\n FRYING DON’TS \n1. Don’t overfill the kettle; above line on rear of tank. \n2. Don’t allow fat in kettle to fry down to the point where there is insufficient fat in which to fry a full load. \n3. Don’t have heat on tubes when they are not entirely covered with frying fat. \n4. Don’t allow fat in kettle to be heated above 375°F and never turn thermostats to 400°F or over, even when bringing up to temperature. \n5. Don’t allow unnecessary moisture or breading material to get into frying kettle. \n6. Don’t allow fat in kettles to remain at frying temperature for long periods of time without frying taking place. \n7. Don’t overload frying kettle with food to be fried. \n8. Don’t pack the food too tightly in the baskets. \n9. Don’t add foreign fats to kettle such as bacon, beef drippings, or waste fat. \n10. Don’t fry bacon in deep frying kettle. \n11. Don’t salt foods over or near the frying kettle. \n12. Don’t allow visible burned particles to remain floating in fry kettle. \n13. Don’t allow exhaust stack accumulations to drip back into the kettle."
+
     with gr.Row():
         with gr.Column(scale=1):
             gr.Image(value="logo.png", show_label=False, interactive=False)
